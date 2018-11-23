7acb4ea9b6bb12c4970ff17ef2f45fbb5500dbfd
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-11-23 09:42:10 +0000

    DIT-515 Add Union

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,10 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "play-reactivemongo"       % "6.2.0",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "3.15.0",
-    "com.amazonaws"           %  "aws-java-sdk-s3"          % "1.11.454"
+    "uk.gov.hmrc"             %% "play-reactivemongo"         % "6.2.0",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "3.15.0",
+    "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.4.0",
+    "com.amazonaws"           %  "aws-java-sdk-s3"            % "1.11.454"
   )
 
   lazy val scope: String = "test,it"
