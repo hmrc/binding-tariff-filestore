496786946d0729c8a701f3790f4d8cc7761e8242
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-11-23 11:36:59 +0000

    DIT-515 Scan Result refactor

--- app/uk/gov/hmrc/bindingtarifffilestore/model/ScanResult.scala
+++ app/uk/gov/hmrc/bindingtarifffilestore/model/ScanResult.scala
@@ -3,27 +3,72 @@ package uk.gov.hmrc.bindingtarifffilestore.model
 import java.time.Instant
 
 import play.api.libs.json._
+import uk.gov.hmrc.bindingtarifffilestore.model
 import uk.gov.hmrc.bindingtarifffilestore.model.FailureReason.FailureReason
 import uk.gov.hmrc.bindingtarifffilestore.model.ScanStatus.ScanStatus
+import uk.gov.hmrc.play.json.Union
 
-case class ScanResult(
-      reference: String,
-      fileStatus: ScanStatus,
-      downloadUrl: Option[String],
-      uploadDetails: Option[UploadDetails],
-      failureDetails: Option[FailureDetails])
-
-object ScanResult {
-  implicit val format = Json.format[ScanResult]
+case class SuccessfulScanResult
+(
+  override val reference: String,
+  downloadUrl: String,
+  uploadDetails: UploadDetails
+) extends ScanResult {
+  override val fileStatus: model.ScanStatus.Value = ScanStatus.READY
 }
 
-case class UploadDetails(uploadTimestamp: Instant, checksum: String)
+object SuccessfulScanResult {
+  implicit val format: OFormat[SuccessfulScanResult] = Json.format[SuccessfulScanResult]
+}
+
+
+
+case class FailedScanResult
+(
+  override val reference: String,
+  failureDetails: FailureDetails
+) extends ScanResult {
+  override val fileStatus: model.ScanStatus.Value = ScanStatus.FAILED
+}
+
+object FailedScanResult {
+  implicit val format: OFormat[FailedScanResult] = Json.format[FailedScanResult]
+}
+
+
+
+sealed trait ScanResult {
+  val reference: String
+  val fileStatus: ScanStatus
+}
+
+object ScanResult {
+  implicit val format: Format[ScanResult] = Union
+    .from[ScanResult]("fileStatus")
+    .and[SuccessfulScanResult](ScanStatus.READY.toString)
+    .and[FailedScanResult](ScanStatus.FAILED.toString)
+    .format
+}
+
+
+
+case class UploadDetails
+(
+  uploadTimestamp:
+  Instant, checksum: String
+)
 
 object UploadDetails {
   implicit val format: OFormat[UploadDetails] = Json.format
 }
 
-case class FailureDetails(failureReason: FailureReason, message: String)
+
+
+case class FailureDetails
+(
+  failureReason: FailureReason,
+  message: String
+)
 
 object FailureDetails {
   implicit val format: OFormat[FailureDetails] = Json.format
