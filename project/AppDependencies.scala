7f4cb0d50de0b3f8ee95b77c12b3d3226f9898c2
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-01-03 22:56:03 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "com.amazonaws"           %  "aws-java-sdk-s3"            % "1.11.470",
+    "com.amazonaws"           %  "aws-java-sdk-s3"            % "1.11.475",
     "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.4.0",
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.4.0",
     "uk.gov.hmrc"             %% "simple-reactivemongo"       % "7.7.0-play-25"
@@ -12,7 +12,7 @@ object AppDependencies {
   lazy val scope: String = "test,it"
 
   val test = Seq(
-    "com.github.tomakehurst"        %  "wiremock"               % "2.19.0"          % scope,
+    "com.github.tomakehurst"        %  "wiremock"               % "2.20.0"          % scope,
     "org.mockito"                   %  "mockito-core"           % "2.23.4"          % scope,
     "org.pegdown"                   %  "pegdown"                % "1.6.0"           % scope,
     "org.scalaj"                    %% "scalaj-http"            % "2.4.1"           % scope,
