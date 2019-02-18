ac3d8538949a9375530d0826502700d8a57cbff5
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-02-18 16:58:51 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
 
   val test = Seq(
     "com.github.tomakehurst"        %  "wiremock"               % "2.21.0"          % scope,
-    "org.mockito"                   %  "mockito-core"           % "2.24.0"          % scope,
+    "org.mockito"                   %  "mockito-core"           % "2.24.5"          % scope,
     "org.pegdown"                   %  "pegdown"                % "1.6.0"           % scope,
     "org.scalaj"                    %% "scalaj-http"            % "2.4.1"           % scope,
     "org.scalatestplus.play"        %% "scalatestplus-play"     % "2.0.1"           % scope,
