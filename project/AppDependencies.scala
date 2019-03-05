a247f9a1579f6ab9d94c01eb0fe996fbe3af08fb
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-05 21:22:23 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "io.megl"                     %% "play-json-extra"            % "2.4.3",
-    "com.amazonaws"               %  "aws-java-sdk-s3"            % "1.11.509",
+    "com.amazonaws"               %  "aws-java-sdk-s3"            % "1.11.511",
     "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.9.0",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
     "uk.gov.hmrc"                 %% "simple-reactivemongo"       % "7.14.0-play-25",
@@ -16,7 +16,7 @@ object AppDependencies {
 
   val test = Seq(
     "com.github.tomakehurst"        %  "wiremock"               % "2.21.0"          % scope,
-    "org.mockito"                   %  "mockito-core"           % "2.24.5"          % scope,
+    "org.mockito"                   %  "mockito-core"           % "2.25.0"          % scope,
     "org.pegdown"                   %  "pegdown"                % "1.6.0"           % scope,
     "org.scalaj"                    %% "scalaj-http"            % "2.4.1"           % scope,
     "org.scalatestplus.play"        %% "scalatestplus-play"     % "2.0.1"           % scope,
