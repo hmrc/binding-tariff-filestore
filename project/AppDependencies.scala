a02fec93aa94f87684e53004b203da280d2af8be
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2019-03-23 16:58:08 +0000

    BAU: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "com.amazonaws"               %  "aws-java-sdk-s3"            % "1.11.524",
     "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.9.0",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc"                 %% "simple-reactivemongo"       % "7.14.0-play-25",
+    "uk.gov.hmrc"                 %% "simple-reactivemongo"       % "7.16.0-play-25",
     "org.apache.httpcomponents"   %  "httpclient"                 % "4.5.7",
     "org.apache.httpcomponents"   %  "httpmime"                   % "4.5.7"
   )
@@ -15,14 +15,14 @@ object AppDependencies {
   lazy val scope: String = "test,it"
 
   val test = Seq(
-    "com.github.tomakehurst"        %  "wiremock"               % "2.22.0"          % scope,
-    "org.mockito"                   %  "mockito-core"           % "2.25.1"          % scope,
-    "org.pegdown"                   %  "pegdown"                % "1.6.0"           % scope,
-    "org.scalaj"                    %% "scalaj-http"            % "2.4.1"           % scope,
-    "org.scalatestplus.play"        %% "scalatestplus-play"     % "2.0.1"           % scope,
-    "uk.gov.hmrc"                   %% "hmrctest"               % "3.6.0-play-25"   % scope,
-    "uk.gov.hmrc"                   %% "http-verbs-test"        % "1.4.0-play-25"   % scope,
-    "uk.gov.hmrc"                   %% "reactivemongo-test"     % "4.9.0-play-25"   % scope
+    "com.github.tomakehurst"        %  "wiremock"               % "2.22.0"         % scope,
+    "org.mockito"                   %  "mockito-core"           % "2.25.1"         % scope,
+    "org.pegdown"                   %  "pegdown"                % "1.6.0"          % scope,
+    "org.scalaj"                    %% "scalaj-http"            % "2.4.1"          % scope,
+    "org.scalatestplus.play"        %% "scalatestplus-play"     % "2.0.1"          % scope,
+    "uk.gov.hmrc"                   %% "hmrctest"               % "3.6.0-play-25"  % scope,
+    "uk.gov.hmrc"                   %% "http-verbs-test"        % "1.4.0-play-25"  % scope,
+    "uk.gov.hmrc"                   %% "reactivemongo-test"     % "4.10.0-play-25" % scope
   )
 
 }
