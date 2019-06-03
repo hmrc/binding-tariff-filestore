998e9c64a42cc138c39b82df72a7ebc9a3da3764
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-06-03 10:27:09 +0100

    DIT - Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
   val compile = Seq(
     "io.megl"                     %% "play-json-extra"            % "2.4.3",
     "com.amazonaws"               %  "aws-java-sdk-s3"            % "1.11.532",
-    "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.10.0",
+    "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.12.0",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc"                 %% "simple-reactivemongo"       % "7.16.0-play-25",
+    "uk.gov.hmrc"                 %% "simple-reactivemongo"       % "7.19.0-play-25",
     "org.apache.httpcomponents"   %  "httpclient"                 % apacheHttpVersion,
     "org.apache.httpcomponents"   %  "httpmime"                   % apacheHttpVersion
   )
@@ -22,9 +22,9 @@ object AppDependencies {
     "org.pegdown"                   %  "pegdown"                % "1.6.0"          % scope,
     "org.scalaj"                    %% "scalaj-http"            % "2.4.1"          % scope,
     "org.scalatestplus.play"        %% "scalatestplus-play"     % "2.0.1"          % scope,
-    "uk.gov.hmrc"                   %% "hmrctest"               % "3.6.0-play-25"  % scope,
-    "uk.gov.hmrc"                   %% "http-verbs-test"        % "1.4.0-play-25"  % scope,
-    "uk.gov.hmrc"                   %% "reactivemongo-test"     % "4.10.0-play-25" % scope
+    "uk.gov.hmrc"                   %% "hmrctest"               % "3.8.0-play-25"  % scope,
+    "uk.gov.hmrc"                   %% "http-verbs-test"        % "1.6.0-play-25"  % scope,
+    "uk.gov.hmrc"                   %% "reactivemongo-test"     % "4.14.0-play-25" % scope
   )
 
 }
