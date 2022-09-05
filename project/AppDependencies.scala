95bf090294d702c3e0d4a95265207720faf0e8c7
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-09-05 15:42:21 +0100

    [DDCE-2637] Further Dep Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,20 +14,20 @@ object AppDependencies {
   )
 
   val compile: Seq[ModuleID] = Seq(
-    "com.amazonaws"             % "aws-java-sdk-s3"            % "1.12.294",
-    "uk.gov.hmrc"               %% "bootstrap-backend-play-28" % "6.4.0",
+    "com.amazonaws"             % "aws-java-sdk-s3"            % "1.12.296",
+    "uk.gov.hmrc"               %% "bootstrap-backend-play-28" % "7.1.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-28"        % mongoHmrcVersion,
     "uk.gov.hmrc"               %% "play-json-union-formatter" % "1.15.0-play-28",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.4",
     "org.apache.httpcomponents" % "httpclient"                 % apacheHttpVersion,
     "org.apache.httpcomponents" % "httpmime"                   % apacheHttpVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "com.github.tomakehurst" % "wiremock-jre8"             % "2.27.2",
+    "com.github.tomakehurst" % "wiremock-jre8"             % "2.33.2",
     "com.typesafe.play"      %% "play-test"                % current ,
     "org.mockito"            % "mockito-core"              % "4.7.0",
     "org.jsoup"              % "jsoup"                     % "1.15.3",
-    "org.pegdown"            % "pegdown"                   % "1.6.0" ,
     "org.scalatest"          %% "scalatest"                % "3.2.13" ,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0" ,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0",
@@ -35,10 +35,7 @@ object AppDependencies {
     "org.scalacheck"         %% "scalacheck"               % "1.16.0",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % mongoHmrcVersion,
-    "org.scalaj"             %% "scalaj-http"              % "2.4.2",
-    //Need to peg this version for wiremock - try removing this on next lib upgrade
-    "org.eclipse.jetty" % "jetty-server"  % jettyVersion,
-    "org.eclipse.jetty" % "jetty-servlet" % jettyVersion
+    "org.scalaj"             %% "scalaj-http"              % "2.4.2"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ silencerDependencies ++ test
