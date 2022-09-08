99893ca3bbf9846334660d6bd0aa1f143912bbe2
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-09-08 12:27:45 +0100

    [DDCE-2637] jetty removed, akka ver, mockito ver and resolvers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
 
   private lazy val apacheHttpVersion = "4.5.13"
   private lazy val mongoHmrcVersion  = "0.71.0"
-  val jettyVersion                   = "9.4.48.v20220622"
   private val silencerVersion        = "1.7.9"
 
   private val silencerDependencies: Seq[ModuleID] = Seq(
@@ -14,7 +13,7 @@ object AppDependencies {
   )
 
   val compile: Seq[ModuleID] = Seq(
-    "com.amazonaws"                 % "aws-java-sdk-s3"           % "1.12.296",
+    "com.amazonaws"                 % "aws-java-sdk-s3"           % "1.12.298",
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % "7.2.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % mongoHmrcVersion,
     "uk.gov.hmrc"                  %% "play-json-union-formatter" % "1.15.0-play-28",
@@ -26,7 +25,7 @@ object AppDependencies {
   val test: Seq[ModuleID]    = Seq(
     "com.github.tomakehurst"  % "wiremock-jre8"            % "2.33.2",
     "com.typesafe.play"      %% "play-test"                % current,
-    "org.mockito"             % "mockito-core"             % "4.7.0",
+    "org.mockito"             % "mockito-core"             % "4.8.0",
     "org.jsoup"               % "jsoup"                    % "1.15.3",
     "org.scalatest"          %% "scalatest"                % "3.2.13",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
